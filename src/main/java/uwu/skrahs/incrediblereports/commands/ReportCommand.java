271fc9592de12e1bb02f2c61651b4580710916ee
thatskai <108898782+thatskai@users.noreply.github.com>
2024-04-14 00:02:41 +0200

    update
    Took 44 seconds

--- src/main/java/uwu/skrahs/incrediblereports/commands/ReportCommand.java
+++ src/main/java/uwu/skrahs/incrediblereports/commands/ReportCommand.java
@@ -1,5 +1,7 @@
 package uwu.skrahs.incrediblereports.commands;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.inject.Inject;
 import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.command.CommandSource;
@@ -9,61 +11,72 @@ import dev.dejvokep.boostedyaml.YamlDocument;
 import net.kyori.adventure.text.Component;
 import uwu.skrahs.incrediblereports.manager.ConfigManager;
 import uwu.skrahs.incrediblereports.utils.ChatUtils;
-import com.velocitypowered.api.permission.PermissionSubject;
 
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class ReportCommand implements SimpleCommand {
 
     private final ProxyServer proxy;
     private final ConfigManager configManager;
+    private final Cache<UUID, Long> cooldown;
 
     @Inject
     public ReportCommand(ProxyServer proxy, ConfigManager configManager) {
         this.proxy = proxy;
         this.configManager = configManager;
+        this.cooldown = CacheBuilder.newBuilder().expireAfterWrite(configManager.getConfig("config").get().getInt("cooldown"), TimeUnit.SECONDS).build();
     }
 
     @Override
     public void execute(Invocation invocation) {
         CommandSource sender = invocation.source();
-        String[] args = invocation.arguments(); // Utilizzare String[] invece di List<String>
+        String[] args = invocation.arguments();
         YamlDocument config = configManager.getConfig("config").get();
 
-        if (!(sender instanceof Player)) {
+        if (!(sender instanceof Player player)) {
             sender.sendMessage(ChatUtils.color(config.getString("messages.player_only_command")));
             return;
         }
 
-        Player player = (Player) sender;
+        if(cooldown.asMap().containsKey(player.getUniqueId())){
+            player.sendMessage(ChatUtils.color(config.getString("messages.cooldown")));
+            return;
+        }
 
         if (args.length < 2) {
             player.sendMessage(ChatUtils.color(config.getString("messages.correct_usage")));
             return;
         }
 
-        String reportedPlayerName = args[0]; // Utilizzare args[0] invece di args.get(0)
-        String reason = String.join(" ", Arrays.copyOfRange(args, 1, args.length)); // Modifica della concatenazione della motivazione
+        String reportedPlayerName = args[0];
+        String reason = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
 
-        // Check if the reported player is online
         Player reportedPlayer = proxy.getPlayer(reportedPlayerName).orElse(null);
         if (reportedPlayer == null) {
             player.sendMessage(ChatUtils.color(String.format(config.getString("messages.player_not_online"), reportedPlayerName)));
             return;
         }
 
-        // Send report message to authorized players
+        if(reportedPlayer == player){
+            player.sendMessage(ChatUtils.color(config.getString("messages.self_report")));
+            return;
+        }
+
         Component reportMessage = ChatUtils.color(config.getString("messages.alert")
                 .replace("%player%", player.getUsername())
                 .replace("%reported%", reportedPlayer.getUsername())
                 .replace("%reason%", reason)
-        );//quando lo metti ti faccio una pull request xd grande fra!
+        );
         proxy.getAllPlayers().stream()
                 .filter(p -> p.hasPermission("incrediblereports.receivereports"))
                 .forEach(p -> p.sendMessage(reportMessage));
+
+        cooldown.put(player.getUniqueId(), config.getInt("cooldown")*1000L);
 
         player.sendMessage(ChatUtils.color(config.getString("messages.report_sent_successfully")));
     }
@@ -72,7 +85,7 @@ public class ReportCommand implements SimpleCommand {
     public List<String> suggest(Invocation invocation) {
         List<String> list = new ArrayList<>();
         for(Player player : proxy.getAllPlayers()){
-            list.add(player.getUsername());//ffatto ma quanto sei ptoetnte, testiamo sul proxy della vps
+            list.add(player.getUsername());
         }
         return list;
     }
